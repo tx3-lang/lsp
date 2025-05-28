6e86c311db804b85f3a03b52614a8695867ba7ef
sofia-bobbiesi <sofiabobbiesi@gmail.com>
2025-05-28 16:15:09 -0300

    feat: add generate_diagram command

---
+++ src/cmds/generate_diagram.rs
@@ -0,0 +1,40 @@
+use serde_json::{json, Value};
+
+use crate::{Context, Error};
+
+pub struct Args {
+    document_url: String,
+}
+
+impl TryFrom<Vec<Value>> for Args {
+    type Error = Error;
+
+    fn try_from(value: Vec<Value>) -> Result<Self, Self::Error> {
+        Ok(Args {
+            document_url: value
+                .get(0)
+                .and_then(|v| v.as_str())
+                .map(|s| s.to_owned())
+                .ok_or(Error::InvalidCommandArgs("document_url".to_string()))?,
+        })
+    }
+}
+
+pub async fn run(
+    context: &Context,
+    args: impl TryInto<Args, Error = Error>,
+) -> Result<Option<Value>, Error> {
+    let args: Args = args.try_into()?;
+
+    let protocol = context.get_document_protocol(&args.document_url)?;
+
+    let ast = protocol.ast().to_owned();
+
+    // let diagram = ast_to_diagram(&ast);
+
+    let out = json!({
+        "diagram": "<h1>Testing</h1>",
+    });
+
+    Ok(Some(out))
+}
